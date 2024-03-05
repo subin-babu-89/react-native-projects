c553ace29acca8e46c9e04ae972a683f801f8e5d
subin-babu-89 <subinbabu89@gmail.com>
2024-03-05 14:00:32 -0800

    add second project-wip

---
+++ 02-card-list-rendering/CardListStyling/components/ElevatedCards.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ElevatedCards() {
+    return (
+        <View>
+            <Text style={styles.headingText}>Elevated Cards</Text>hub project 
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    headingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        paddingHorizontal: 10,
+    },
+})
